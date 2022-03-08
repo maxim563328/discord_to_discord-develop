17e6d215393e2d4a3d501f9831c588cfdfb8fccc
Максим <maximfilonenko2@gmail.com>
2022-03-09 00:13:18 +0700

    small

--- discord_to_discord/main.py
+++ discord_to_discord/main.py
@@ -8,16 +8,18 @@ import commands_module as commands_module
 import random
 import string
 import os
+import re
 
 from disnake import ChannelType
 from discord import File
 from discord.ext import commands, tasks
 from telethon.tl.types import InputMessagePinned
-from telethon.tl.functions.channels import GetChannelsRequest
+from telethon.tl.functions.messages import CheckChatInviteRequest
 from telethon.tl.types import InputChannel
-from telethon.tl.types.messages import ChatFull
 
-from telethon import TelegramClient, types, errors, functions, utils
+from telethon import TelegramClient, types, errors, functions, events
+
+URL_REGEX = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 
 intents = discord.Intents.all()
 client = commands.Bot(
@@ -37,8 +39,6 @@ users_data = {
 
 
 tg_pinned_groups = {}
-
-
 with sq.connect(r'database.db') as con:
     con = con
     cur = con.cursor()
@@ -69,7 +69,8 @@ def chunk_text(text: str, chunk_lenght: int) -> list:
     return result
 
 
-@tasks.loop(seconds=5.0)
+# Задача, проверяющая каждые 2 минуты новое закрепленное сообщения в каналаз из базы данных
+@tasks.loop(minutes=2.0)
 async def check_telegram_pin_msg():
     cur.execute("SELECT * FROM tg_channels")
     res = cur.fetchall()
@@ -94,6 +95,7 @@ async def check_telegram_pin_msg():
         await asyncio.sleep(2)
 
 
+# Старт дискорд бота
 @client.event
 async def on_ready():
     name = client.user.name + '#' + client.user.discriminator
@@ -105,14 +107,23 @@ async def on_ready():
 #     Имя бота: {name}
 #
 """)
+    async with client_tg:
+        channel = await client_tg.get_entity("https://t.me/lobsters_chat")
+
+    # Старт телеграм 'бота', требуется для начала отслеживания ивентов
+    await client_tg.start()
+    await client_tg.run_until_disconnected()
 
 
 @client.event
 async def on_message(message):
+    # Команда для добавления телеграм канала с закрепленными сообщениями и отслеживанием контента
     if message.content.split(' ')[0] == "$add-tg-channel":
         if message.author.id not in users_data["accessed_accounts"].keys():
             return
         command_text = message.content.split(' ')
+
+        # Проверка второго аргумента команды, на тип (@username или https)
         checker = commands_module.check_command_tg_type(command_text)
         if checker == 0:
             await message.channel.send("**Вы не указали канал телеграм ❌**")
@@ -140,6 +151,10 @@ async def on_message(message):
             await message.channel.send("**Чат, приглашение которого вы присылали, не существует ❌**")
             return
         if type(data) != types.Channel:
+            print(data)
+            if data == 101:
+                await message.channel.send("**Вы отправили приглашение на беседу ❌**")
+                return
             if data.channel is False:
                 await message.channel.send("**Чат, приглашение на которой вы прислали, не является каналом ❌**")
                 return
@@ -161,6 +176,7 @@ async def on_message(message):
             con.commit()
         await message.channel.send(f"**Канал `{data.title}` успешно сохранён ✅**")
 
+    # Команда для удаления сервера из отслеживания закрепленных сообщений
     if message.content.split(' ')[0] == "$rem-server":
         if message.author.id not in users_data["accessed_accounts"].keys():
             return
@@ -182,11 +198,15 @@ async def on_message(message):
                     (int(command_text[1])))
         con.commit()
         await message.channel.send("**Удаление успешно выполнено ✅**")
+
+    # Команда для добавления сервера в отслеживание закрепленных сообщений
     if message.content.split(' ')[0] == "$add-server":
         if message.author.id not in users_data["accessed_accounts"].keys():
             return
         command_text = message.content.split(' ')
         data = commands_module.check_valid_add(command_text)
+
+        # Обработка ошибок, набранной команды
         if data == 0:
             await message.channel.send(f"**Какой то из аргументов упущен, принято аргументов - {len(command_text) - 1}/3 ❌**")
             return
@@ -208,6 +228,8 @@ async def on_message(message):
         if data == 12:
             await message.channel.send(f"**Бот не находится на сервере с ID - `{command_text[3]}`, пожалуйста, проверьте корректность ID ❌**")
             return
+
+        # Результат команды
         if commands_module.check_in_data_base(data["server_get"], "server_list", "server_get") == 1:
             await message.channel.send("**Данный сервер уже есть в базе данных ❌**")
             return
@@ -248,16 +270,65 @@ async def on_message(message):
         if int(msg["id"]) == message.id:
             message_ = msg
 
+    # В случае наличия вложения в сообщении
     if message_["attachments"] != []:
         path = 'attachments/' + download_attachment(message_["attachments"][0]["proxy_url"], message_[
                                                     "attachments"][0]["filename"], message_["attachments"][0]["content_type"], message_["attachments"][0]["size"])
         await channel.send("**[Новое сообщение]**\nСервер | **{}**\nКанал | `{}`\nАвтор | `{}`\nСодержание: {}".format(message.author.guild.name, message.channel.name, message.author, message_["content"]), file=File(path))
         os.remove(path)
         return
+    # Обычное сообщение
     await asyncio.sleep(1)
     await channel.send("**[Новое сообщение]**\nСервер | **{}**\nКанал | `{}`\nАвтор | `{}`\nСодержание: {}".format(message.author.guild.name, message.channel.name, message.author, message_["content"]))
 
 
+# Получение каналов для ивента on_message, в которых отслеживаются посты
+def get_channels_for_event(event_type: str = 'GetChannelPost'):
+    result = []
+    if event_type == 'GetChannelPost':
+        cur.execute("SELECT * FROM tg_channels")
+        data = cur.fetchall()
+        for item in data:
+            result.appen(item[0])
+    return tuple(result)
+
+
+# Событие для отслеживания новых постов канала
+@client_tg.on(events.NewMessage(incoming=False, outgoing=True, chats=get_channels_for_event()))
+async def tg_main_OnMessage(event):
+    channel_discord = client.get_channel(696285756225749026)
+    channel_data = await client_tg.get_entity(event.peer_id.channel_id)
+    channel_name = channel_data.title
+    channel_id = channel_data.id
+    await channel_discord.send(f"""
+**[Новое сообщение в канале телеграм]
+Название канала: `{channel_name}`
+ID канала: `{channel_id}`
+Содержание:**
+{event.message.message}
+
+
+""")
+
+
+# Событие для ловли ссылок в чате телеграм
+@client_tg.on(events.NewMessage(incoming=False, outgoing=True, chats=(760992172)))
+async def new_msg(event):
+    content = event.message.message.split("\n")
+    channel = client.get_channel(696285756225749026)
+    result = ''
+    for word in content:
+        word = word.split(" ")
+        for letter in word:
+            if re.match(URL_REGEX, letter):
+                result += letter + '\n'
+    author = await client_tg.get_entity(event.message.from_id.user_id)
+    if result.split("\n") == ['']:
+        return
+    await channel.send(f"Отправитель: `@{author.username}`\nСообщение:\n{result}")
+
+
+# Скачать вложения дискорд сообщения
 def download_attachment(url: str, filename: str, content_type: str, size: int):
     file_id = "_" + "".join(random.choices(string.ascii_uppercase +
                             string.digits + string.ascii_lowercase, k=6))
@@ -267,30 +338,29 @@ def download_attachment(url: str, filename: str, content_type: str, size: int):
     return filename
 
 
+# Проверка телеграм ссылки на канал, если она в формате https
 async def check_valid_tg_add_link(command_text):
     async with client_tg:
         if len(command_text) < 2:
             return 0
         hash_tg = command_text[1].split('/')[-1]
-        if len(hash_tg) == 16:
-            try:
-                channel = await client_tg.get_entity(f"@{hash_tg}")
-            except:
-                try:
-                    result = await client_tg(functions.messages.CheckChatInviteRequest(hash=hash_tg))
-                except errors.InviteHashExpiredError:
-                    return 11
-                except errors.InviteHashInvalidError:
-                    return 12
 
-                return result
-            else:
-                channel = await client_tg.get_entity(f"@{hash_tg}")
-                return channel
-        channel = await client_tg.get_entity(f"@{hash_tg}")
+        try:
+            channel = await client_tg.get_entity(f"@{hash_tg}")
+        except:
+            try:
+                print(hash_tg)
+                channel = await client_tg(functions.messages.CheckChatInviteRequest(hash=command_text[1]))
+            except errors.InviteHashExpiredError:
+                print("error 11")
+                return 11
+            except errors.InviteHashInvalidError:
+                print("error 12")
+                return 12
         return channel
 
 
+# Проверка телеграм ссылки на канал, если она в формате @username
 async def check_valid_tg_add_decorator(command_text):
     async with client_tg:
         if len(command_text) < 2:
@@ -302,6 +372,7 @@ async def check_valid_tg_add_decorator(command_text):
         return peer
 
 
+# Получение закрепленных сообщений канала по хэшу и id (берутся из базы данных)
 async def get_pinned_tg_message(id_: int, hash_: int):
     async with client_tg:
         channel = InputChannel(id_, hash_)
